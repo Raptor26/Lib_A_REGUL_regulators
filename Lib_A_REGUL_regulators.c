738a09476a4e2238fc97371a4dbb771c26d795ff
Isaev Mickle <mrraptor26@gmail.com>
2018-04-23 11:56:35 +0300

    feature/style (Lib_A_REGUL_regulators) добавлен PID регулятор
    - Изменено форматирование кода, поэтому при просмотре через difftool видно множество изменений

--- Lib_A_REGUL_regulators.c
+++ Lib_A_REGUL_regulators.c
@@ -41,12 +41,12 @@ float g_b1;
 
 /******************************************************************************/
 // 	Секция прототипов локальных функций
-float PowerForIBSC(
-                   float basis,
-                   float exponent);
-float RestrictionSaturation(
-                            float value,
-                            float saturation);
+float PowerForIBSC (
+                    float basis,
+                    float exponent);
+float RestrictionSaturation (
+                             float value,
+                             float saturation);
 /******************************************************************************/
 
 
@@ -75,86 +75,86 @@ float RestrictionSaturation(
  * @note	см. eq. 4.45 - 4.53 в документе "Design and control of quadrotors
  *			with application to autonomous flying"
  */
-float REGUL_IntegralBackStep
-(REGUL_integ_back_step_s *pStruct,
- float e1,
- float phi_d,
- float omega_x)
+float
+REGUL_IntegralBackStep (REGUL_integ_back_step_s *pStruct,
+                        float e1,
+                        float phi_d,
+                        float omega_x)
 {
-    /*---- |Begin| --> Дифференцирование phi_d (т.е. желаемого положения) ----*/
-    /* Установка периода дифференцирования */
-    pStruct->phi_d_derivStruct.dT = pStruct->dT;
+  /*---- |Begin| --> Дифференцирование phi_d (т.е. желаемого положения) ----*/
+  /* Установка периода дифференцирования */
+  pStruct->phi_d_derivStruct.dT = pStruct->dT;
 
-    /* Дифференцирование желаемого положения методом 1-го порядка */
-    float phi_d_deriv = DIFF_FindDifferent1(&pStruct->phi_d_derivStruct,
-                                            phi_d);
-    /*---- |End  | <-- Дифференцирование phi_d (т.е. желаемого положения) ----*/
+  /* Дифференцирование желаемого положения методом 1-го порядка */
+  float phi_d_deriv = DIFF_FindDifferent1 (&pStruct->phi_d_derivStruct,
+                                           phi_d);
+  /*---- |End  | <-- Дифференцирование phi_d (т.е. желаемого положения) ----*/
 
-    /* Расчет интегральной состоавляющей (смотри комментарий к eq. 4.46) */
-    pStruct->chi += e1 * pStruct->lambda * pStruct->dT;
+  /* Расчет интегральной состоавляющей (смотри комментарий к eq. 4.46) */
+  pStruct->chi += e1 * pStruct->lambda * pStruct->dT;
 
-    /*	Ограничение насыщения интегральной составляющей */
-    pStruct->chi = RestrictionSaturation(pStruct->chi,
-                                         pStruct->saturation);
-
-    /* Расчет желаемой скорости (eq. 4.46) */
-    float omega_xd = pStruct->c1 * e1
-            + phi_d_deriv
-            + pStruct->lambda * pStruct->chi;
-
-    /* Ограничение насыщения желаемой скорости */
-    omega_xd = RestrictionSaturation(omega_xd,
-                                     pStruct->saturation);
-
-    /* Расчет ошибки между желаемой скоростью и фактической (eq. 4.48) */
-    float e2 = omega_xd - omega_x;
-
-    /* Коэффициент "b1" должен быть только положительным */
-    if (pStruct->b1 < 0.0f)
-    {
-        pStruct->b1 *= -1.0f;
-    }
-
-    /* Расчет управляющего воздействия (eq. 4.53) */
-    float returnValue = (1 / pStruct->b1)
-            * (1 - pStruct->c1 * pStruct->c1 + pStruct->lambda)
-            * e1
-            + ((pStruct->c1 + pStruct->c2) * e2)
-            - (pStruct->c1 * pStruct->lambda * pStruct->chi);
-
-    /* |Begin| --> Взятие второй производной от e1 ---------------------------*/
-    pStruct->e1_FirstDerivStruct.dT = pStruct->dT;
-    pStruct->e1_SecontDerivStruct.dT = pStruct->dT;
-
-    /* Нахождение первой производной от e1 */
-    float e1DerivTemp = DIFF_FindDifferent1(&pStruct->e1_FirstDerivStruct,
-                                            e1);
-
-    /* Нахождение второй производной от e1 */
-    //    e1DerivTemp = DIFF_FindDifferent1(&pStruct->e1_SecontDerivStruct,
-    //                                      e1DerivTemp);
-
-    /* Добавление второй производной от ошибки положения в результат
-     * работы Integral Back Step Control */
-    returnValue += e1DerivTemp * pStruct->e1SecondDerivCoeff;
-    /* |End  | <-- Взятие второй производной от e1 ---------------------------*/
-
-    /* Ограничение насыщения выходного параметра */
-    returnValue = RestrictionSaturation(returnValue,
+  /*	Ограничение насыщения интегральной составляющей */
+  pStruct->chi = RestrictionSaturation (pStruct->chi,
                                         pStruct->saturation);
 
-#    if	defined __REGUL_REGULATORS_DEBUG__
-    g_e1 = e1;
-    g_e2 = e2;
-    g_IntegralBackStepReturnValue = returnValue;
-    g_omega_x = omega_x;
-    g_omega_xd = omega_xd;
-    g_phi_d_deriv = phi_d_deriv;
-    g_chi = pStruct->chi;
-    g_b1 = pStruct->b1;
-#    endif
+  /* Расчет желаемой скорости (eq. 4.46) */
+  float omega_xd = pStruct->c1 * e1
+          + phi_d_deriv
+          + pStruct->lambda * pStruct->chi;
 
-    return returnValue;
+  /* Ограничение насыщения желаемой скорости */
+  omega_xd = RestrictionSaturation (omega_xd,
+                                    pStruct->saturation);
+
+  /* Расчет ошибки между желаемой скоростью и фактической (eq. 4.48) */
+  float e2 = omega_xd - omega_x;
+
+  /* Коэффициент "b1" должен быть только положительным */
+  if (pStruct->b1 < 0.0f)
+    {
+      pStruct->b1 *= -1.0f;
+    }
+
+  /* Расчет управляющего воздействия (eq. 4.53) */
+  float returnValue = (1 / pStruct->b1)
+          * (1 - pStruct->c1 * pStruct->c1 + pStruct->lambda)
+          * e1
+          + ((pStruct->c1 + pStruct->c2) * e2)
+          - (pStruct->c1 * pStruct->lambda * pStruct->chi);
+
+  /* |Begin| --> Взятие второй производной от e1 ---------------------------*/
+  pStruct->e1_FirstDerivStruct.dT = pStruct->dT;
+  pStruct->e1_SecontDerivStruct.dT = pStruct->dT;
+
+  /* Нахождение первой производной от e1 */
+  float e1DerivTemp = DIFF_FindDifferent1 (&pStruct->e1_FirstDerivStruct,
+                                           e1);
+
+  /* Нахождение второй производной от e1 */
+  //    e1DerivTemp = DIFF_FindDifferent1(&pStruct->e1_SecontDerivStruct,
+  //                                      e1DerivTemp);
+
+  /* Добавление второй производной от ошибки положения в результат
+   * работы Integral Back Step Control */
+  returnValue += e1DerivTemp * pStruct->e1SecondDerivCoeff;
+  /* |End  | <-- Взятие второй производной от e1 ---------------------------*/
+
+  /* Ограничение насыщения выходного параметра */
+  returnValue = RestrictionSaturation (returnValue,
+                                       pStruct->saturation);
+
+#if	defined __REGUL_REGULATORS_DEBUG__
+  g_e1 = e1;
+  g_e2 = e2;
+  g_IntegralBackStepReturnValue = returnValue;
+  g_omega_x = omega_x;
+  g_omega_xd = omega_xd;
+  g_phi_d_deriv = phi_d_deriv;
+  g_chi = pStruct->chi;
+  g_b1 = pStruct->b1;
+#endif
+
+  return returnValue;
 }
 
 /**
@@ -167,86 +167,115 @@ float REGUL_IntegralBackStep
  * @note	см. eq. 4.45 - 4.53 в документе "Design and control of quadrotors
  *			with application to autonomous flying"
  */
-void REGUL_Init_IntergralBackStep(
-                                  REGUL_integ_back_step_s *pStruct)
+void
+REGUL_Init_IntergralBackStep (
+                              REGUL_integ_back_step_s *pStruct)
 {
-    pStruct->dT = 0.0f;
-    pStruct->c1 = 1.0f;
-    pStruct->c2 = 0.0f;
-    pStruct->lambda = 0.0f;
-    pStruct->b1 = 1.0f;
-    pStruct->e1PowCoeff = 1.0f;
-    pStruct->e2PowCoeff = 1.0f;
-    pStruct->saturation = 0.0f;
-    pStruct->chi = 0.0f;
-    pStruct->omega_xd = 0.0f;
-    pStruct->phi_d_t1 = 0.0f;
-    pStruct->phi_d_deriv = 0.0f;
-    pStruct->e1SecondDerivCoeff = 0.0f;
+  pStruct->dT = 0.0f;
+  pStruct->c1 = 1.0f;
+  pStruct->c2 = 0.0f;
+  pStruct->lambda = 0.0f;
+  pStruct->b1 = 1.0f;
+  pStruct->e1PowCoeff = 1.0f;
+  pStruct->e2PowCoeff = 1.0f;
+  pStruct->saturation = 0.0f;
+  pStruct->chi = 0.0f;
+  pStruct->omega_xd = 0.0f;
+  pStruct->phi_d_t1 = 0.0f;
+  pStruct->phi_d_deriv = 0.0f;
+  pStruct->e1SecondDerivCoeff = 0.0f;
 
-    /* Значения переключателей */
-    pStruct->tumblers.e1TakeModuleFlag = REGUL_DIS;
-    pStruct->tumblers.phi_d_its_e1Flag = REGUL_DIS;
-    pStruct->tumblers.enablePowFunctFlag = REGUL_DIS;
+  /* Значения переключателей */
+  pStruct->tumblers.e1TakeModuleFlag = REGUL_DIS;
+  pStruct->tumblers.phi_d_its_e1Flag = REGUL_DIS;
+  pStruct->tumblers.enablePowFunctFlag = REGUL_DIS;
 }
 
 /**
  * @brief	Функция ПИ регулятора с ограничением насыщения интегральной составляющей;
  * @see	А.С. Анучин "Системы управления электроприводами" стр. 209
  */
-float REGUL_PI_regulator(REGUL_pid_regulator_s *pSturct, float error)
+float
+REGUL_PI_regulator (REGUL_pid_regulator_s *pSturct, float error)
 {
-    float propCorrect;
-    float piCorrect;
-    float errorAndSaturetionDiff;
+  float propCorrect;
+  float piCorrect;
+  float errorAndSaturetionDiff;
 
-    //	Расчет пропорциональной коррекции
-    propCorrect = error * pSturct->kp;
+  //	Расчет пропорциональной коррекции
+  propCorrect = error * pSturct->kp;
 
-    //	Расчет интегральной коррекции;
-    pSturct->integral += error * pSturct->ki * pSturct->dT;
+  //	Расчет интегральной коррекции;
+  pSturct->integral += error * pSturct->ki * pSturct->dT;
 
-    //	Расчет ПИ регулятора;
-    piCorrect = propCorrect + pSturct->integral;
+  //	Расчет ПИ регулятора;
+  piCorrect = propCorrect + pSturct->integral;
 
-    piCorrect += pSturct->integralAfterCorrect;
+  piCorrect += pSturct->integralAfterCorrect;
 
-    // Рассчет во сколько раз сигнал получился больше насыщения;
-    errorAndSaturetionDiff = fabsf(piCorrect) / fabsf(pSturct->saturation);
+  // Рассчет во сколько раз сигнал получился больше насыщения;
+  errorAndSaturetionDiff = fabsf (piCorrect) / fabsf (pSturct->saturation);
 
-    //	Масштабирование входного сигнала;
-    //	error *= errorAndSaturetionDiff;
+  //	Масштабирование входного сигнала;
+  //	error *= errorAndSaturetionDiff;
 
-    //	Расчет пропорциональной коррекции после коррекции насыщения
-    propCorrect = error * pSturct->kp;
+  //	Расчет пропорциональной коррекции после коррекции насыщения
+  propCorrect = error * pSturct->kp;
 
-    //	Расчет интегральной коррекции;
-    pSturct->integralAfterCorrect += error * pSturct->ki * pSturct->dT;
+  //	Расчет интегральной коррекции;
+  pSturct->integralAfterCorrect += error * pSturct->ki * pSturct->dT;
 
-    //	Ограничение насыщения интегральной составляющей;
-    if (pSturct->integralAfterCorrect > pSturct->saturation)
+  //	Ограничение насыщения интегральной составляющей;
+  if (pSturct->integralAfterCorrect > pSturct->saturation)
     {
-        pSturct->integralAfterCorrect = pSturct->saturation;
+      pSturct->integralAfterCorrect = pSturct->saturation;
     }
-    else if (pSturct->integralAfterCorrect < (-pSturct->saturation))
+  else if (pSturct->integralAfterCorrect < (-pSturct->saturation))
     {
-        pSturct->integralAfterCorrect = -pSturct->saturation;
-    }
-
-    //	Расчет ПИ регулятора;
-    piCorrect = propCorrect + pSturct->integral;
-
-    //	Ограничение насыщения ПИ регулятора;
-    if (piCorrect > pSturct->saturation)
-    {
-        piCorrect = pSturct->saturation;
-    }
-    else if (piCorrect < (-pSturct->saturation))
-    {
-        piCorrect = -pSturct->saturation;
+      pSturct->integralAfterCorrect = -pSturct->saturation;
     }
 
-    return piCorrect;
+  //	Расчет ПИ регулятора;
+  piCorrect = propCorrect + pSturct->integral;
+
+  //	Ограничение насыщения ПИ регулятора;
+  if (piCorrect > pSturct->saturation)
+    {
+      piCorrect = pSturct->saturation;
+    }
+  else if (piCorrect < (-pSturct->saturation))
+    {
+      piCorrect = -pSturct->saturation;
+    }
+
+  return piCorrect;
+}
+
+float
+REGUL_PIDRegulator (
+                    REGUL_pid_regulator_s *pData_struct,
+                    float error,
+                    float derivError)
+{
+  float returnValue = error * pData_struct->kp;
+
+  pData_struct->integral += error * pData_struct->ki;
+
+  if (error < pData_struct->integralSaturation)
+    {
+      pData_struct->integral
+              = RestrictionSaturation
+              (pData_struct->integral,
+               pData_struct->integralSaturation);
+
+      returnValue += pData_struct->integral;
+    }
+  
+  returnValue += derivError * pData_struct->kd;
+  
+  returnValue = RestrictionSaturation (returnValue, pData_struct->saturation);
+  
+  return returnValue;
 }
 /*============================================================================*/
 
@@ -258,31 +287,32 @@ float REGUL_PI_regulator(REGUL_pid_regulator_s *pSturct, float error)
  * @rapam
  * @return
  */
-float PowerForIBSC(
-                   float basis,
-                   float exponent)
+float
+PowerForIBSC (
+              float basis,
+              float exponent)
 {
-    //	Если степень, в которую необходимо возвести "basis" не равна "1.0f",
-    //	"-1.0f" или "0.0f":
-    if ((exponent != 1.0f)
-            || (exponent != -1.0f)
-            || (exponent != 0.0f))
+  //	Если степень, в которую необходимо возвести "basis" не равна "1.0f",
+  //	"-1.0f" или "0.0f":
+  if ((exponent != 1.0f)
+      || (exponent != -1.0f)
+      || (exponent != 0.0f))
     {
-        //	Если "basis" положительное число;
-        if (basis >= 0.0f)
+      //	Если "basis" положительное число;
+      if (basis >= 0.0f)
         {
-            //	"e1PowCoeff" берется по модулю;
-            basis = powf(basis, fabsf(exponent));
+          //	"e1PowCoeff" берется по модулю;
+          basis = powf (basis, fabsf (exponent));
         }
-            //	Иначе (если "basis" отрицательное число):
-        else
+        //	Иначе (если "basis" отрицательное число):
+      else
         {
-            //	Результат возведения в степень модуля числа "basis" умножается на "-1.0f";
-            //	"exponent" берется по модулю;
-            basis = (powf(fabsf(basis), fabsf(exponent))) * -1.0f;
+          //	Результат возведения в степень модуля числа "basis" умножается на "-1.0f";
+          //	"exponent" берется по модулю;
+          basis = (powf (fabsf (basis), fabsf (exponent))) * -1.0f;
         }
     }
-    return basis;
+  return basis;
 }
 
 /**
@@ -293,26 +323,27 @@ float PowerForIBSC(
  * 							переменную "value"
  * @return	Значение переменной "value", с учетом значения насыщения "saturation";
  */
-float RestrictionSaturation(
-                            float value,
-                            float saturation)
+float
+RestrictionSaturation (
+                       float value,
+                       float saturation)
 {
-    /*	Ограничение насыщения выходного параметра */
-    //	Если переменная насыщения не равна "0.0f":
-    if (saturation != 0.0f)
+  /*	Ограничение насыщения выходного параметра */
+  //	Если переменная насыщения не равна "0.0f":
+  if (saturation != 0.0f)
     {
-        //	Если выходное значение больше положительного значения переменной насыщения:
-        if (value > saturation)
+      //	Если выходное значение больше положительного значения переменной насыщения:
+      if (value > saturation)
         {
-            value = saturation;
+          value = saturation;
         }
-            //	Если выходное значение меньше отрицательного значения переменной насыщения:
-        else if (value < (-saturation))
+        //	Если выходное значение меньше отрицательного значения переменной насыщения:
+      else if (value < (-saturation))
         {
-            value = -saturation;
+          value = -saturation;
         }
     }
-    return value;
+  return value;
 }
 /******************************************************************************/
 
--- Lib_A_REGUL_regulators.h
+++ Lib_A_REGUL_regulators.h
@@ -41,153 +41,153 @@
 
 /******************************************************************************/
 //  Секция определения типов
+
 /**
  * @brief	Структура, содержащая переменные для работы IBSC регулятора;
  */
-typedef struct
-{
-	/*---- |Begin| --> Коэффициенты регулятора -------------------------------*/
-	/**
-	 * @brief	Коэффициент коррекции ошибки положения;
-	 * @warning Это поле должно быть определено при инициализации стуктуры;
-	 */
-	float c1;
+typedef struct {
+  /*---- |Begin| --> Коэффициенты регулятора -------------------------------*/
+  /**
+   * @brief	Коэффициент коррекции ошибки положения;
+   * @warning Это поле должно быть определено при инициализации стуктуры;
+   */
+  float c1;
 
-	/**
-	 * @brief	Коэффициент коррекции ошибки скорости;
-	 * @warning	Это поле должно быть определено при инициализации стуктуры;
-	 */
-	float c2;
+  /**
+   * @brief	Коэффициент коррекции ошибки скорости;
+   * @warning	Это поле должно быть определено при инициализации стуктуры;
+   */
+  float c2;
 
-	/**
-	 * @brief 	Коэффициент;
-	 * @warning	Это поле должно быть определено при инициализации стуктуры;
-	 * @see		eq. 4.53;
-	 */
-	float b1;
+  /**
+   * @brief 	Коэффициент;
+   * @warning	Это поле должно быть определено при инициализации стуктуры;
+   * @see		eq. 4.53;
+   */
+  float b1;
 
-	/**
-	 * @brief	Коэффициент для второй производной от e1
-	 */
-	float e1SecondDerivCoeff;
-	/*---- |End  | <-- Коэффициенты регулятора -------------------------------*/
+  /**
+   * @brief	Коэффициент для второй производной от e1
+   */
+  float e1SecondDerivCoeff;
+  /*---- |End  | <-- Коэффициенты регулятора -------------------------------*/
 
-	/**
-	 * @brief	Коэффициент коррекции интегральной составляющей ошибки положения;
-	 * @warning	Это поле должно быть определено при инициализации стуктуры;
-	 */
-	float lambda;
+  /**
+   * @brief	Коэффициент коррекции интегральной составляющей ошибки положения;
+   * @warning	Это поле должно быть определено при инициализации стуктуры;
+   */
+  float lambda;
 
-	/**
-	 * @brief	Интегральная состоавляющая от ошибки положения;
-	 */
-	float chi;
+  /**
+   * @brief	Интегральная состоавляющая от ошибки положения;
+   */
+  float chi;
 
-	/**
-	 * @brief	Желаемая угловая скорость;
-	 */
-	float omega_xd;
+  /**
+   * @brief	Желаемая угловая скорость;
+   */
+  float omega_xd;
 
-	/**
-	 * @brief	Производная от желаемого положения;
-	 */
-	float phi_d_deriv;
+  /**
+   * @brief	Производная от желаемого положения;
+   */
+  float phi_d_deriv;
 
-	/**
-	 * @brief	Желаемое положение на шаге <t-1>;
-	 */
-	float phi_d_t1;
+  /**
+   * @brief	Желаемое положение на шаге <t-1>;
+   */
+  float phi_d_t1;
 
-	/**
-	 * @brief	Величина шага для нахождения производной;
-	 */
-	float dT;
+  /**
+   * @brief	Величина шага для нахождения производной;
+   */
+  float dT;
 
-	/**
-	 * @brief	Степерь, в которую необходимо возвести ошибку "e1";
-	 */
-	float e1PowCoeff;
+  /**
+   * @brief	Степерь, в которую необходимо возвести ошибку "e1";
+   */
+  float e1PowCoeff;
 
-	/**
-	 * @brief	Степень, в которую необходимо возвести ошибку "e2"
-	 */
-	float e2PowCoeff;
+  /**
+   * @brief	Степень, в которую необходимо возвести ошибку "e2"
+   */
+  float e2PowCoeff;
 
-	/**
-	 * @brief	Максимальное значение по модулю выходного параметра регулятора;
-	 */
-	float saturation;
+  /**
+   * @brief	Максимальное значение по модулю выходного параметра регулятора;
+   */
+  float saturation;
 
-	/**
-	 * @brief	Структура для дифференцирования переменной phi_d
-	 */
-	DIFF_differentiator_1_s phi_d_derivStruct;
+  /**
+   * @brief	Структура для дифференцирования переменной phi_d
+   */
+  DIFF_differentiator_1_s phi_d_derivStruct;
 
-	/*
-	 * @brief	Структура для дифференцирования e1
-	 */
-	DIFF_differentiator_1_s e1_FirstDerivStruct,
-	    e1_SecontDerivStruct;
+  /*
+   * @brief	Структура для дифференцирования e1
+   */
+  DIFF_differentiator_1_s e1_FirstDerivStruct,
+  e1_SecontDerivStruct;
 
-	/**
-	 * @brief	Переключатели режима работы регулятора;
-	 */
-	struct
-	{
-		/**
-		 * @brief	Взятие ошибки "e1" по модулю;
-		 */
-		size_t e1TakeModuleFlag;
+  /**
+   * @brief	Переключатели режима работы регулятора;
+   */
+  struct {
+    /**
+     * @brief	Взятие ошибки "e1" по модулю;
+     */
+    size_t e1TakeModuleFlag;
 
-		/**
-		 * @brief	Интерпретация входного параметра "phi_d" как "e1"
-		 * @see		brief функции REGUL_IntegralBackStep()
-		 * @note	Если phi_d_its_e1Flag == 1, то параметр "phi" в функции
-		 * 			REGUL_IntegralBackStep() не используется
-		 */
-		size_t phi_d_its_e1Flag;
+    /**
+     * @brief	Интерпретация входного параметра "phi_d" как "e1"
+     * @see		brief функции REGUL_IntegralBackStep()
+     * @note	Если phi_d_its_e1Flag == 1, то параметр "phi" в функции
+     * 			REGUL_IntegralBackStep() не используется
+     */
+    size_t phi_d_its_e1Flag;
 
-		/**
-		 * @brief	Флаг разрешает возведение в степень переменных e1, e2;
-		 */
-		size_t enablePowFunctFlag;
+    /**
+     * @brief	Флаг разрешает возведение в степень переменных e1, e2;
+     */
+    size_t enablePowFunctFlag;
 
-	} tumblers; /*	Переключатели режима работы регулятора */
+  } tumblers; /*	Переключатели режима работы регулятора */
 
 } REGUL_integ_back_step_s;
 
-typedef struct
-{
-	float kp;
+typedef struct {
+  float kp;
 
-	float ki;
+  float ki;
 
-	float kd;
+  float kd;
 
-	/**
-	 * @brief	Интегральная состоавляющая ошибки;
-	 */
-	float integral;
+  /**
+   * @brief	Интегральная состоавляющая ошибки;
+   */
+  float integral;
 
-	/**
-	 * @brief	Интегральная состоавляющая ошибки после коррекции насыщения;
-	 */
-	float integralAfterCorrect;
+  /**
+   * @brief	Интегральная состоавляющая ошибки после коррекции насыщения;
+   */
+  float integralAfterCorrect;
 
-	/**
-	 * @brief	Ошибка за предыдущий момент времени;
-	 */
-	float error_t1;
+  /**
+   * @brief	Ошибка за предыдущий момент времени;
+   */
+  float error_t1;
 
-	/**
-	 * @brief	Величина шага для нахождения производной;
-	 */
-	float dT;
+  /**
+   * @brief	Величина шага для нахождения производной;
+   */
+  float dT;
 
-	/**
-	 * @brief	Ограничение насыщения;
-	 */
-	float saturation;
+  /**
+   * @brief	Ограничение насыщения;
+   */
+  float saturation;
+  
+  float integralSaturation;
 } REGUL_pid_regulator_s;
 /******************************************************************************/
 
@@ -204,23 +204,32 @@ extern float g_phi_d_deriv;
 extern float g_chi;
 extern float g_b1;
 #endif
-/******************************************************************************/
+/*******F***********************************************************************/
 
 
 /******************************************************************************/
 //  Секция прототипов глобальных функций
-extern float REGUL_IntegralBackStep(
-                                    REGUL_integ_back_step_s *pStruct,
-                                    float phi_d,
-                                    float phi,
-                                    float omega_x);
+extern float
+REGUL_IntegralBackStep(
+        REGUL_integ_back_step_s *pStruct,
+        float phi_d,
+        float phi,
+        float omega_x);
 
-extern void REGUL_Init_IntergralBackStep(
-                                         REGUL_integ_back_step_s *pStruct);
+extern void
+REGUL_Init_IntergralBackStep(
+        REGUL_integ_back_step_s *pStruct);
 
-extern float REGUL_PI_regulator(
-                                REGUL_pid_regulator_s *pSturct,
-                                float error);
+extern float
+REGUL_PI_regulator(
+        REGUL_pid_regulator_s *pSturct,
+        float error);
+
+extern float
+REGUL_PIDRegulator(
+        REGUL_pid_regulator_s *pData_struct,
+        float error,
+        float derivError);
 /******************************************************************************/
 
 
